7b036b321810fba65aa41f8e01f5436f1e1514dd
ehrid <zygota.v2@gmail.com>
2014-04-11 11:46:39 +0200

    initial commit

---
+++ MonsterMaster/src/org/monstermaster/splashscreen/LogoApplication.java
@@ -0,0 +1,26 @@
+package org.monstermaster.splashscreen;
+
+import org.monstermaster.R;
+import org.monstermaster.main.MainActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @author horodysk
+ */
+public class LogoApplication extends SplashscreanActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setLogo(R.drawable.logo_monstermaster);
+    }
+
+    @Override
+    void startActivity() {
+        Intent intent = new Intent(LogoApplication.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
